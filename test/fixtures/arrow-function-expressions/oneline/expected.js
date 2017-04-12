1c74c91a34dc22096f072ccbebe8bca3e6702b26
William C. Johnson <wcjohnson@oigroup.net>
2017-04-12 14:08:00 -0400

    Added fixtures for oneline statement parsing

--- test/fixtures/arrow-function-expressions/oneline/expected.js
+++ test/fixtures/arrow-function-expressions/oneline/expected.js
@@ -1,3 +1,31 @@
 (function () {
   return 1;
-});
+});(function () {
+  return 1;
+});() => {
+  for (let _i = 0, _arr = Object.keys(obj), _len = _arr.length; _i < _len; _i++) {
+    const k = _arr[_i];
+    f(k);
+  }
+};() => {
+  throw new Error();
+};(() => {
+  throw new Error();
+})();
+
+const f = () => {
+  throw new Error();
+};
+
+f();
+
+(function () {
+  return 1;
+});func();
+
+() => {
+  for (let _i2 = 0, _arr2 = Object.keys(obj), _len2 = _arr2.length; _i2 < _len2; _i2++) {
+    const k = _arr2[_i2];
+    f(k);
+  }
+};func();
