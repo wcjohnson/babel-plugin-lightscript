8da2b578de32c2d19bde636146a32067c440038e
William C. Johnson <wcjohnson@oigroup.net>
2017-05-25 23:43:15 -0400

    Update fixture for new positional node generation

--- test/fixtures/match/binding/expected.js
+++ test/fixtures/match/binding/expected.js
@@ -1,15 +1,11 @@
 if (x === 1) {
-  const y = x;
-  y;
+  const y = x;y;
 } else if (x === 2) {
-  const { key } = x;
-  key;
+  const { key } = x;key;
 } else if (x === 3) {
   const [first] = x;
-
   const result = process(first);
   processAgain(result);
 } else {
-  const other = x;
-  other;
+  const other = x;other;
 }
