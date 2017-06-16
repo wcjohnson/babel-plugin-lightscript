2d9fb272dced16ae708d1154a5c4725325fe97f1
William C. Johnson <wcjohnson@oigroup.net>
2017-06-15 21:49:21 -0400

    Fix for numeric literal in destructuring

--- test/fixtures/match_v4/kitchen-sink/expected.js
+++ test/fixtures/match_v4/kitchen-sink/expected.js
@@ -1,6 +1,6 @@
 function _hasProps(obj) { if (obj == null) return false; if (typeof obj !== "object" && typeof obj !== "function") return false; for (var i = arguments.length - 1; i > 0; i--) if (!(arguments[i] in obj)) return false; return true; }
 
-function _matches(discriminant, target) {}
+function _matches(discriminant, atom) { return discriminant === atom; }
 
 if (_matches(x, Atom)) {
   1;
