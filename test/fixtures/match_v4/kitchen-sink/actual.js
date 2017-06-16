b92471ce0398b471adc960f60b85c96b5c8addc5
William C. Johnson <wcjohnson@oigroup.net>
2017-06-16 17:27:37 -0400

    Options improvements
    - Pattern matching defaults to `v4`
    - Flow and JSX can be disabled via flags
    - flippedImports can be enabled via flag
    - API for deciding if a file is lightscript

--- test/fixtures/match_v4/kitchen-sink/actual.js
+++ test/fixtures/match_v4/kitchen-sink/actual.js
@@ -1,6 +1,6 @@
 match x:
   | Atom: 1
   | ~Predicate(): 2
-  | if preGuard: /regex/: 3
+  | if preGuard when /regex/: 3
   | ~Predicate() with { pattern }: 4
-  | if Preguard: ~Predicate1() and ~Predicate2(), String as { x } if x > 4: 5
+  | if Preguard when ~Predicate1() and ~Predicate2(), String as { x } if x > 4: 5
