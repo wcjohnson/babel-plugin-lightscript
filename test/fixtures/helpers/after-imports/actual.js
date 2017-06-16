fedd8e0ac6bcbbe42075abb19081023b98c3e4f7
William C. Johnson <wcjohnson@users.noreply.github.com>
2017-06-16 17:28:19 -0400

    Pattern Matching v4 (#4)
    * Pattern Matching v4
    
    * Fix for numeric literal in destructuring
    
    * Runtime (wip)
    
    * Infrastructure cleanup
    
    - Eliminate `getTypes()`
    - Collect pass-specific data in `compilerState`
    - Unify code for adding new imports
    - Support for runtime
    
    * Use runtime for match helpers
    
    * Add runtime dep; update fixtures
    
    * Expose `useRequire` option
    
    * Add test
    
    * v2.0.0-beta.7
    
    * Options improvements
    
    - Pattern matching defaults to `v4`
    - Flow and JSX can be disabled via flags
    - flippedImports can be enabled via flag
    - API for deciding if a file is lightscript

--- test/fixtures/helpers/after-imports/actual.js
+++
@@ -1,5 +0,0 @@
-import x from "y"
-
-match z:
-  | with [w]: "1"
-  | with {a}: "2"
--- test/fixtures/helpers/scope/actual.js
+++
@@ -1,9 +0,0 @@
-i = 1
-obj = {}
-minLength = 1
-maxLength = 3
-arr = []
-
-match x:
-  | with { z }: z
-  | with [ w ]: w
