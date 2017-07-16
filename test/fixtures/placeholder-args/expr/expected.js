cd625f1bc9a1bac978c5c725ee029b690b050696
William C. Johnson <wcjohnson@oigroup.net>
2017-07-16 19:41:02 -0400

    Initial placeholderArgs implementation + testing

---
+++ test/fixtures/placeholder-args/expr/expected.js
@@ -0,0 +1,5 @@
+x.map(function (_arg, _arg2) {
+  return _arg2 + _arg;
+});
+
+y.map((_arg3, _arg4) => _arg4 + _arg3);
---
+++ test/fixtures/placeholder-args/positional/expected.js
@@ -0,0 +1,9 @@
+(function (_arg, _arg2) {
+  return _arg2._arg;
+});(function (_arg3) {
+  return _arg3._arg3;
+});(function (_arg4, _arg5) {
+  return _arg5._arg5._arg4._arg4;
+});(function (_arg6, _arg7) {
+  return _arg7(_arg6);
+});
