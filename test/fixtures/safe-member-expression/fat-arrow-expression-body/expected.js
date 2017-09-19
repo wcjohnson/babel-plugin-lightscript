8e3a39125323aa9877422a4c7326e0590fec3074
William C. Johnson <wcjohnson@oigroup.net>
2017-09-18 23:10:51 -0400

    Fix for SafeMemberExpressions in ArrowFunctionExpressions
    See https://github.com/wcjohnson/lightscript/issues/23

---
+++ test/fixtures/safe-member-expression/fat-arrow-expression-body/expected.js
@@ -0,0 +1,11 @@
+() => _ == null ? null : _.a;
+
+() => {
+  var _ref;
+
+  return (_ref = _.a) == null ? null : _ref.a;
+};() => {
+  var _ref2;
+
+  return (_ref2 = f()) == null ? null : _ref2.x;
+};
