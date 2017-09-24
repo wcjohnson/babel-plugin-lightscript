cd7cdb0d646fa1bc70593746b565acbacd8fe3cf
William C. Johnson <wcjohnson@oigroup.net>
2017-09-24 19:11:17 -0400

    Move safe transforms to a separate AST visitor pass

--- test/fixtures/safe-member-expression/calls-after/expected.js
+++ test/fixtures/safe-member-expression/calls-after/expected.js
@@ -1,11 +1,11 @@
-var _ref, _ref2, _ref3;
+var _a$b, _a$b$c, _c;
 
 a == null ? void 0 : a.b();
 
-(_ref = a == null ? void 0 : a.b()) == null ? void 0 : _ref.c().d.e();
+a == null ? void 0 : (_a$b = a.b()) == null ? void 0 : _a$b.c().d.e();
 
-(_ref2 = a == null ? void 0 : a.b().c) == null ? void 0 : _ref2.d().e.f();
+a == null ? void 0 : (_a$b$c = a.b().c) == null ? void 0 : _a$b$c.d().e.f();
 
 a == null ? void 0 : c(a.b);
 
-(_ref3 = a == null ? void 0 : c(a.b)) == null ? void 0 : e(_ref3.d);
+(_c = a == null ? void 0 : c(a.b)) == null ? void 0 : e(_c.d);
