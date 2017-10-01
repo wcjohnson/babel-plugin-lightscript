8c6b3fc073058a9171a42f3235508f9318e3bfdc
William C. Johnson <wcjohnson@oigroup.net>
2017-09-30 21:44:58 -0400

    `whiteblockPreferred` mode

--- test/fixtures/match/braces/expected.js
+++ test/fixtures/match/braces/expected.js
@@ -2,7 +2,7 @@ const _hasProps = require("@oigroup/lightscript-runtime/hasProps");
 if (x === 1) {
   true;
 } else if (test(it) && _hasProps(x, "x")) {
-  const { x } = x;x;
+  const { x } = x;({ x });
 } else {
   false;
 }
