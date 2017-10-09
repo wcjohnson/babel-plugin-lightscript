09ccb87a37649f7d3cb51a0e2165167f6268b2d9
William C. Johnson <wcjohnson@oigroup.net>
2017-10-08 21:30:22 -0400

    Whiteblock early termination testing

---
+++ test/fixtures/whitespace/block-termination/actual.js
@@ -0,0 +1,18 @@
+{
+  entry: func() ->
+    result,
+  entry2: something,
+  entry3: reactFunc() ->
+    <SomeJSX>
+    </SomeJSX>,
+  entry4: somethingElse
+}
+
+[
+  func() ->
+    result,
+  something
+]
+
+call! func() ->
+  result, arg2
