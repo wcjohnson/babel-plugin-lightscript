5e48ddd185c8e909934d2547e3eeedefce9491bc
William C. Johnson <wcjohnson@oigroup.net>
2017-10-12 16:56:18 -0400

    Register function decls on entry
    - Fixes https://github.com/wcjohnson/lightscript/issues/49

--- test/fixtures/implicit-returns/destructure-kitchen-sink/expected.js
+++ test/fixtures/implicit-returns/destructure-kitchen-sink/expected.js
@@ -1,4 +1,4 @@
 function f(x) {
-  const [a, b = c, { d, e: f = g, h: [i, j, { k: { l, m } = n }] }] = x;
-  return [a, b, { d, f, h: [i, j, { k: { l, m } }] }];
+  const [a, b = c, { d, e: o = g, h: [i, j, { k: { l, m } = n }] }] = x;
+  return [a, b, { d, o, h: [i, j, { k: { l, m } }] }];
 }
